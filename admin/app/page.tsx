9bc6f8d3506c91a0e75f2cea7e74d2351a481707
25lp3625 User <25lp3625@administrators-iMac.local>
2026-03-31 12:11:08 +0800

    dutuu

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,7 +1,14 @@
+import { SignIn } from "@/components/SignIn";
+
 export default function Home() {
-  return <>
-  <div>
-    
-  </div>
-  </>
+  return (
+    <div>
+      <div>
+        <SignIn />
+      </div>
+      <div>
+        <img src="/bg-img.png" alt="BatBat" />
+      </div>
+    </div>
+  );
 }
---
+++ admin/components/SignIn.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { signIn } from "@/app/api/sign-in";
+import { Input } from "@base-ui/react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async () => {
+    const credentials = {
+      email,
+      password,
+    };
+    try {
+      const data = await signIn(credentials);
+      localStorage.setItem("token", data?.accessToken);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Input
+        placeholder="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Sign In</Button>
+    </div>
+  );
+};
